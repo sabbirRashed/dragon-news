1a786f1c293bd0f30f72bd090e412100d2f115de
sabbirRashed <srsabbirrahman12@gmail.com>
2026-05-11 13:27:35 +0600

    added breaking news animation title and navbar

---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const BreakingNews = () => {
+    const breakingNews = [
+        {
+            id: 1,
+            title: "Massive Storm Causes Flooding in Coastal Areas"
+        },
+        {
+            id: 2,
+            title: "New AI Technology Transforms Web Development"
+        },
+        {
+            id: 3,
+            title: "Global Markets Rise After Economic Recovery Report"
+        }
+    ];
+
+
+    return (
+        <div className='flex justify-between items-center gap-4 bg-gray-200 p-4 rounded-sm container mx-auto'>
+            <button className='btn bg-red-500 text-white'>Latest</button>
+            <Marquee 
+            speed={100}
+            pauseOnHover={true}>
+                {
+                    breakingNews.map(item => (
+                        <h2 
+                        key={item.id}
+                        className='mx-2'>{item.title}</h2>
+                    ))
+                }
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import userAvater from '@/assets/user.png'
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>navbar</h2>
+        <div className='flex justify-between items-center container mx-auto mt-4'>
+            <div>
+
+            </div>
+
+            <ul className='flex justify-between items-center text-gray-600 gap-6'>
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/about"}>About</Link></li>
+                <li><Link href={"/career"}>Career</Link></li>
+            </ul>
+
+            <div className='flex justify-between items-center gap-2'>
+                <Image
+                    src={userAvater}
+                    alt='user avater'></Image>
+                <Link href={'/login'}>
+                    <button className='btn'>Login</button>
+                </Link>
+            </div>
         </div>
     );
 };
